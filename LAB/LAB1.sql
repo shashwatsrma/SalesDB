e57698c1115240bb64ff78a82d51591ba37161b4
shashwatsrma <Shashwat.srma@gmail.com>
2026-01-16 17:15:10 +0545

    Created table Customers

--- LAB/LAB1.sql
+++ LAB/LAB1.sql
@@ -1,2 +1,13 @@
 CREATE DATABASE sec_bct_2079;
 USE sec_bct_2079;
+
+
+create table customer(
+customer_id int not null primary key,
+first_name varchar(30) not null,
+last_name varchar(30),
+phone varchar(15) unique,
+city varchar(30),
+country varchar(30) default 'NEPAL');
+
+select * from customer
